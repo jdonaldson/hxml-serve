558a41401efa1e18f25b62ba25b6b8e92c59522c
Justin Donaldson <donaldson@bigml.com>
2012-07-03 09:32:38 -0700

    adding in some basic methods

--- Index.hx
+++ Index.hx
@@ -1,6 +1,26 @@
+import haxe.web.Dispatch;
+import neko.Web;
+import neko.Sys;
+import neko.Lib;
+import neko.FileSystem;
+using Lambda;
 class Index {
     static function main() {
-        trace("hello world!");
+        Dispatch.run(Web.getURI(), Web.getParams(), new Api());
     }
+}
 
+class Api{
+    public function new(){}
+    public function doHxml(?file:String){
+        var hxmls = new List<String>();
+        if (file == '' || file == null){
+            var dirs = FileSystem.readDirectory(Sys.getCwd());
+            hxmls = dirs.filter(function(x) return ~/\.hxml$/.match(x));
+        }
+        Lib.print(hxmls.join(", "));
+    }
+    public function doDefault(){
+        trace('default');
+    }
 }
